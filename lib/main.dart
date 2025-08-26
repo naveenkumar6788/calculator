ec477caa01258692257ff99e930f4d52d1b65e01
naveenkumar6788 <naveenkumardasari6788@gmail.com>
2025-08-26 10:36:41 +0530

    new line by pandubhai

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:calculator/app.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-
+// added by pandubhai
 void main() {
   runApp(DevicePreview(builder: (context)=> MyApp()));
 }
